8644159b9aecd8c2ac2fb5da930f7a60fc7ee9f0
SoaringEaqle <158627775+SoaringEaqle@users.noreply.github.com>
2025-09-23 13:37:21 -0600

    Completes Prep exercise 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        do
+        {
+            //Console.Write("What is the magic number? ");
+            //String magicNumber = Console.ReadLine();
+            Random rand = new Random();
+            int num = rand.Next(1, 11);
+
+            int guess;
+            int guesses = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                guesses++;
+
+
+                if (num == guess)
+                {
+                    Console.WriteLine($"You guessed it in {guesses} tries!");
+                    
+                }
+                else if (num > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            while(num != guess);
+            
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+            
+        } 
+        while (response == "yes");
     }
 }
