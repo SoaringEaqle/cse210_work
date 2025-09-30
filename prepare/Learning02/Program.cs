6e0f3ddb70c2527fadfe4fb86b8ae227b2dbecb4
SoaringEaqle <158627775+SoaringEaqle@users.noreply.github.com>
2025-09-30 14:23:28 -0600

    Finishes Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using Learning02;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2000;
+        job1._endYear = 2010;
+        job2._jobTitle = "Development Lead";
+        job2._company = "Electronic Arts";
+        job2._startYear = 2010;
+        job2._endYear = 2020;
+       
+        Resume resume = new Resume();
+        resume._name = "Camden Nair";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
---
+++ prove/Develop02/School.cs
@@ -0,0 +1,13 @@
+namespace Develop02;
+
+public class School
+{
+    public int _numStudents;
+    public int _numTeachers;
+    public string _name;
+
+    public void Register()
+    {
+        //Do something;
+    }
+}
