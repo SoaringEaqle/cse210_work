60fac5a109f0ea1ab4f1ced8e523a75474db13cb
SoaringEaqle <158627775+SoaringEaqle@users.noreply.github.com>
2025-10-28 13:35:44 -0600

    Finishes Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        MathAssignment test = new MathAssignment("Words Name", "Topic", "1.4", "6-10 Evens");
+        Console.WriteLine(test.GetSummary());
+        Console.WriteLine(test.GetHomeworkList());
+
+
+        WritingAssignment test1 = new WritingAssignment("Words Name", "Topic", "Essay Title");
+        Console.WriteLine(test1.GetSummary());
+        Console.WriteLine(test1.GetWritingInformation());
+
+        
     }
+
 }
